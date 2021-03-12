8b05d5fddb8cb70fa5f774c1498c3a69420b8571
Brandon Ly <bl4720@rit.edu>
2021-03-11 20:16:12 -0800

    first commit

---
+++ events/client/ready.js
@@ -0,0 +1,3 @@
+module.exports = () => {
+    console.log('Ranna Bot is online!')
+}
---
+++ events/guild/message.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+module.exports = (Discord, client, message) => {
+    if(!message.content.startsWith(process.env.PREFIX) || message.author.bot) return;
+
+    const args = message.content.slice(process.env.PREFIX.length).split(/ +/);
+    const cmd = args.shift().toLowerCase();
+
+    const command = client.commands.get(cmd);
+
+    if(command) command.execute(client, message, args, Discord);
+}
